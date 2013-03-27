a2e8f6f1910222c47b956463eec7a4df56c65ba3
Pascal Precht <pascal.precht@googlemail.com>
2013-03-27 16:22:37 +0100

    initial commit. Boom.

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function (grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    }
+  });
+};
---
+++ ngTranslate/translate.js
@@ -0,0 +1,1 @@
+angular.module('ngTranslate', ['ng']);
