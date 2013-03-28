41309df717ef482889f8a9206030443024b53771
Pascal Precht <pascal.precht@googlemail.com>
2013-03-28 11:53:13 +0100

    Setup  as a service

--- ngTranslate/translate.js
+++ ngTranslate/translate.js
@@ -1,1 +1,39 @@
-angular.module('ngTranslate', ['ng']);
+angular.module('ngTranslate', ['ng']).config(['$provide', function ($provide) {
+
+  $TranslateProvider = function () {
+
+    $translationTable = {};
+
+    this.translations = function (translationTable) {
+      if (translationTable) {
+        $translationTable = translationTable;
+      } else {
+        return $translationTable;
+      }
+    };
+
+    this.$get = ['$interpolate', function ($interpolate) {
+
+      $translate = function (translationId, interpolateParams) {
+        var translation = $translationTable[translationId];
+
+        if (translation) {
+          return $interpolate(translation)(interpolateParams);
+        }
+        return translation;
+      };
+      return $translate;
+    }];
+  };
+
+  $provide.provider('$translate', $TranslateProvider);
+}]);
+
+angular.module('ngTranslate').filter('translate', ['$parse', '$translate', function ($parse, $translate) {
+  return function (translationId, interpolateParams) {
+    if (!angular.isObject(interpolateParams)) {
+      interpolateParams = $parse(interpolateParams)();
+    }
+    return $translate(translationId, interpolateParams);
+  };
+}]);
