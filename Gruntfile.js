c8093a77c26500d4913f9e6ff99512bd0ef4873b
Pascal Precht <pascal.precht@googlemail.com>
2013-04-25 23:10:25 +0200

    feat(conventional-changelogs): Add grunt-conventional-changelog task
    Add Brian Ford's grunt-conventional-changelog task to automate changelog generation.
    
    Closes #11

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-conventional-changelog');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -48,6 +49,11 @@ module.exports = function (grunt) {
       unit: {
         configFile: 'karma.conf.js',
         singleRun: true
+      }
+    },
+    changelog: {
+      options: {
+        dest: 'CHANGELOG.md'
       }
     }
   });
