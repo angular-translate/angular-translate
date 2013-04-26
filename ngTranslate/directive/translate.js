3d7b8c085283d70e6a6431dfcd4500602f28009e
lpaini <lpaini@despegar.com>
2013-04-26 11:21:10 -0300

    Fixed the $watch in the case is inside another directive.

--- ngTranslate/directive/translate.js
+++ ngTranslate/directive/translate.js
@@ -1,33 +1,35 @@
 angular.module('ngTranslate')
 
-.directive('translate', ['$filter', '$interpolate', function ($filter, $interpolate) {
+    .directive('translate', ['$filter', '$interpolate', function ($filter, $interpolate) {
 
-  var translate = $filter('translate');
+        var translate = $filter('translate');
 
-  return {
-    restrict: 'A',
-    scope: true,
-    link: function linkFn(scope, element, attr) {
+        return {
+            restrict: 'A',
+            scope: true,
+            link: function linkFn(scope, element, attr) {
 
-      attr.$observe('translate', function (translationId) {
-        if (angular.equals(translationId , '')) {
-          scope.translationId = $interpolate(element.text())(scope.$parent);
-        } else {
-          scope.translationId = translationId;
-        }
-      });
+                attr.$observe('translate', function (translationId) {
+                    if (angular.equals(translationId, '')) {
+                        scope.translationId = $interpolate(element.text())(scope.$parent);
+                    } else {
+                        scope.translationId = translationId;
+                    }
+                });
 
-      attr.$observe('values', function (interpolateParams) {
-        scope.interpolateParams = interpolateParams;
-      });
+                attr.$observe('values', function (interpolateParams) {
+                    scope.interpolateParams = interpolateParams;
+                });
 
-      scope.$on('translationChangeSuccess', function () {
-        element.html(translate(scope.translationId, scope.interpolateParams));
-      });
+                scope.$on('translationChangeSuccess', function () {
+                    element.html(translate(scope.translationId, scope.interpolateParams));
+                });
 
-      scope.$watch('translationId + interpolateParams', function () {
-        element.html(translate(scope.translationId, scope.interpolateParams));
-      });
-    }
-  };
-}]);
+                scope.$watch('translationId + interpolateParams', function (newValue, oldValue) {
+                    if (newValue !== oldValue) {
+                        element.html(translate(scope.translationId, scope.interpolateParams));
+                    }
+                });
+            }
+        };
+    }]);
