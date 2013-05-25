f552742560ab510d2e324a17cbfeb4327737aa20
Pascal Precht <pascal.precht@googlemail.com>
2013-05-25 12:45:41 +0200

    chore(extensions): remove missing translation handler log service

--- test/unit/translateServiceSpec.js
+++ test/unit/translateServiceSpec.js
@@ -353,57 +353,5 @@ describe('pascalprecht.translate', function () {
         expect($translate.uses()).toEqual($translate.preferredLanguage());
       });
     });
-
-  });
-
-  describe('missingTranslationHandler', function () {
-
-    describe('useMissingTranslationHandler()', function () {
-
-      beforeEach(module('pascalprecht.translate', function ($translateProvider) {
-        $translateProvider.useMissingTranslationHandler('$translateMissingTranslationHandlerLog');
-      }));
-
-      it('should use given missing translation handler service', function () {
-        inject(function ($translate, $log) {
-          spyOn($log, 'warn');
-          $translate('MISSING_TRANSLATION_ID');
-          expect($log.warn).toHaveBeenCalled();
-        });
-      });
-
-      it('should log the right message', function () {
-        inject(function($translate, $log) {
-          spyOn($log, 'warn');
-          $translate('MISSING_TRANSLATION_ID');
-          expect($log.warn)
-            .toHaveBeenCalledWith('Translation for MISSING_TRANSLATION_ID doesn\'t exist');
-        });
-      });
-    });
-
-    describe('useMissingTranslationHandlerLog()', function () {
-
-      beforeEach(module('pascalprecht.translate', function ($translateProvider) {
-        $translateProvider.useMissingTranslationHandlerLog();
-      }));
-
-      it('should use $log service to log missing translation message', function () {
-        inject(function ($translate, $log) {
-          spyOn($log, 'warn');
-          $translate('MISSING_TRANSLATION_ID');
-          expect($log.warn).toHaveBeenCalled();
-        });
-      });
-
-      it('should log the right message', function () {
-        inject(function($translate, $log) {
-          spyOn($log, 'warn');
-          $translate('MISSING_TRANSLATION_ID');
-          expect($log.warn)
-            .toHaveBeenCalledWith('Translation for MISSING_TRANSLATION_ID doesn\'t exist');
-        });
-      });
-    });
   });
 });
