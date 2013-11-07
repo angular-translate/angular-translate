9a3af3cc68533f836f8781192a8e71289b353692
Michael Lehmann <lehmamic@Michael-Lehmanns-iMac.local>
2013-11-07 02:03:19 +0100

    Introduce a grunt task responsible to create the nuget package with nuget for mono

--- Gruntfile.js
+++ Gruntfile.js
@@ -453,7 +453,8 @@ module.exports = function (grunt) {
     'build:loader_static_files',
     'build:loader_url',
     'build:storage_cookie',
-    'build:storage_local'
+    'build:storage_local',
+    'build:nuget'
   ]);
 
   grunt.registerTask('build:core', [
@@ -519,6 +520,28 @@ module.exports = function (grunt) {
     'ngmin:storage_local',
     'uglify:storage_local'
   ]);
+
+  grunt.registerTask('build:nuget', 'Create a nuget package', function() {
+    var done = this.async;
+                     
+    grunt.util.spawn({
+      cmd: 'mono',
+      args: [
+        'nuget/nuget.exe',
+        'pack',
+        'angular-translate.nuspec',
+        '-OutputDirectory',
+        'dist',
+        '-Version', //override the version with whatever is currently defined in package.json
+        grunt.config.get('pkg').version]
+    }, function(error, result) {
+       if (error) {
+          grunt.log.error(error);
+       } else {
+          grunt.log.write(result);
+       }
+    });
+  });
 
 
   // For development purpose.
