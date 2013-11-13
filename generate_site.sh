3c9d4eba8b0c7bf6b690e814e36c63e8ced2dc27
Pascal Precht <pascal.precht@googlemail.com>
2013-11-13 14:47:55 +0100

    chore(docs): ukrainian docs preparation

--- generate_site.sh
+++ generate_site.sh
@@ -1,6 +1,6 @@
 #!bin/bash/ -e
 
-mkdir -p site/docs/en site/docs/ru site/docs/de
+mkdir -p site/docs/en site/docs/ru site/docs/de site/docs/uk
 
 grunt ngdocs
 grunt copy:logos
@@ -18,6 +18,12 @@ grunt ngdocs --lang=de
 grunt copy:logos
 grunt copy:docs_assets
 mv tmp/* site/docs/de
+rm -rf tmp
+
+grunt ngdocs --lang=uk
+grunt copy:logos
+grunt copy:docs_assets
+mv tmp/* site/docs/uk
 rm -rf tmp
 
 cp docs/html/index.html site
