98f74e7b0ece32d731e62dc9b475bb48f02d0d72
Thorsten Spaeth <info@conserata.com>
2014-03-21 16:04:33 +0100

    feat(missingFallbackDefaultText): enables a feature to return a default text for displaying in case of missing translations including fallback stack
    In case you have defined a missingTranslationHandlerFactory, you now have the possibility to define a return value which is set instead of the translationId.
    This gives a lot of flexibility for certain use cases.
    Example factory:
            $provide.factory('customTranslationHandler', function () {
              return function (translationID, uses, fallbacks) {
                if (fallbacks === true) {
                  return 'NO KEY FOUND';
                }
              };
            });

--- src/service/translate.js
+++ src/service/translate.js
@@ -998,7 +998,17 @@ angular.module('pascalprecht.translate').provider('$translate', ['$STORAGE_KEY',
           );
         } else {
           // No translation found in any fallback language
-          deferred.resolve(translationId);
+
+          // If we have a handler factory - we might also call it here to determine if it provides
+          // a default text for a translationid that can't be found anywhere in our tables
+          if ($missingTranslationHandlerFactory) {
+            var resultString = $injector.get($missingTranslationHandlerFactory)(translationId, $uses, true);
+            if (resultString !== undefined) {
+              deferred.resolve(resultString);
+            }
+          } else {
+            deferred.resolve(translationId);
+          }
         }
         return deferred.promise;
       };
@@ -1079,7 +1089,7 @@ angular.module('pascalprecht.translate').provider('$translate', ['$STORAGE_KEY',
           // Now, if there is a registered handler for missing translations and no
           // asyncLoader is pending, we execute the handler
           if ($missingTranslationHandlerFactory && !pendingLoader) {
-            $injector.get($missingTranslationHandlerFactory)(translationId, $uses);
+            $injector.get($missingTranslationHandlerFactory)(translationId, $uses, false);
           }
 
           // since we couldn't translate the inital requested translation id,
