c71e2584dd6b48c97d7096a9ff603b32e74d1001
knalli <knallisworld@googlemail.com>
2014-04-21 22:52:45 +0200

    fix(): fix npe on empty strings (trim())
    solves #435
    solves again #281, #281, #298

--- src/service/translate.js
+++ src/service/translate.js
@@ -734,7 +734,9 @@ angular.module('pascalprecht.translate').provider('$translate', ['$STORAGE_KEY',
         var deferred = $q.defer();
 
         // trim off any whitespace
-        translationId = translationId.trim();
+        if (translationId) {
+          translationId = translationId.trim();
+        }
 
         var promiseToWaitFor = (function () {
           var promise = $preferredLanguage ?
@@ -1511,7 +1513,10 @@ angular.module('pascalprecht.translate').provider('$translate', ['$STORAGE_KEY',
           return translationId;
         }
 
-        translationId = translationId.trim();
+        // trim off any whitespace
+        if (translationId) {
+          translationId = translationId.trim();
+        }
 
         var result, possibleLangKeys = [];
         if ($preferredLanguage) {
