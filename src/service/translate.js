9baca06c8024efcb20a32bd205734d3a6fbeb597
Francisc Romano <franz.r@gmail.com>
2014-06-06 12:35:53 +0300

    Refactored for readability

--- src/service/translate.js
+++ src/service/translate.js
@@ -953,13 +953,13 @@ angular.module('pascalprecht.translate').provider('$translate', ['$STORAGE_KEY',
         if ($translationTable.hasOwnProperty(langKey)) {
           deferred.resolve($translationTable[langKey]);
           return deferred.promise;
-        } else if(!langKey || !langPromises[langKey]) {
-        	deferred.reject();
-        } else {
-          langPromises[langKey].then(function (data) {
+        } else if(langKey && langPromises[langKey]) {
+        	langPromises[langKey].then(function (data) {
             translations(data.key, data.table);
             deferred.resolve(data.table);
           }, deferred.reject);
+        } else {
+          deferred.reject();
         }
         return deferred.promise;
       };
