dd9a7d3917c1d725b402bcc11298b56999f166fb
knalli <knallisworld@googlemail.com>
2014-09-07 18:39:56 +0200

    chore(tests): fix typo in midway setup
    Credits to @NickHeiner

--- karma.midway.conf.js
+++ karma.midway.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
 
     files: [
       'bower_components/angular/angular.js',
-      'bower_components/ngMidwayTester/Source/ngMidwayTester.js',
+      'bower_components/ngMidwayTester/src/ngMidwayTester.js',
       'src/translate.js',
       'bower_components/angular-translate-interpolation-default/angular-translate-interpolation-default.js',
       'src/**/*.js',
