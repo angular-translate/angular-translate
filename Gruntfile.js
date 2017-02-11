37190c766a0c09a92677080e26f791f4ea7b0e3f
knalli <knallisworld@googlemail.com>
2017-02-11 23:15:21 +0100

    chore(): improve internal scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -647,6 +647,7 @@ module.exports = function (grunt) {
 
 
   grunt.registerTask('default', ['jshint:all', 'karma']);
+  grunt.registerTask('lint', ['jshint:all']);
   grunt.registerTask('test', ['jshint:all', 'karma:unit', 'karma:midway']);
   grunt.registerTask('install-test', ['bower-install-simple']);
 
